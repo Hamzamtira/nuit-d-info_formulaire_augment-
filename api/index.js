3717d0935bb2529c26d8d3086db2fc5ef4f32200
Hamza Mtira <hamzamtira477@gmail.com>
2025-12-05 05:24:02 +0100

    Add Vercel configuration files

---
+++ api/index.js
@@ -0,0 +1,3 @@
+// Point d'entrée pour Vercel - redirige vers Backend
+module.exports = require('../Backend/server.js');
+
---
+++ api/server.js
@@ -0,0 +1,3 @@
+// Point d'entrée server.js pour Vercel
+module.exports = require('../Backend/server.js');
+
